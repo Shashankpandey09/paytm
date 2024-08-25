9d111d92c6303f6e1c359398e4c8f149befb0369
Shashankpandey09 <137607589+Shashankpandey09@users.noreply.github.com>
2024-08-25 15:53:09 -0800

    created authMiddleware ,handled updating data

---
+++ backend/middleware.js
@@ -0,0 +1,25 @@
+const { JWT_SECRET_KEY } = require("./config");
+const JWT=require('jsonwebtoken');
+
+const authMiddleware = (req, res, next) => {
+    const AuthHeader = req.headers.authorization;
+
+    if (AuthHeader && AuthHeader.startsWith('Bearer ')) {
+        const token = AuthHeader.split(' ')[1];
+
+        if (token) {
+            JWT.verify(token, JWT_SECRET_KEY, (err, decodedData) => {
+                if (err) {
+                    return res.status(403).json({ message: "Invalid token", error: err });
+                }
+                req.user = decodedData;
+                next();
+            });
+        } else {
+            return res.status(403).json({ message: "Token not valid" });
+        }
+    } else {
+        return res.status(403).json({ message: "Authorization header missing or malformed" });
+    }
+};
+module.exports=authMiddleware;
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,3 +1,85 @@
 const express = require("express");
-const UserRouter=express.Router();
-module.exports=UserRouter;
+const JWT = require("jsonwebtoken");
+const { USER } = require("../db");
+const { JWT_SECRET_KEY } = require("../config");
+const authMiddleware = require("../middleware");
+const UserRouter = express.Router();
+
+const signUpSchema = Zod.object({
+  username: string().email(),
+  firstName: string(),
+  lastName: string(),
+  password: string(),
+});
+
+UserRouter.post("/signup", async (req, res) => {
+  const body = req.body;
+  const { success } = signUpSchema.safeParse(body);
+  if (!success) {
+    return res.json({
+      message: "Email already taken / Incorrect inputs",
+    });
+  }
+  const existingUser = await USER.findOne({ username: req.body.username });
+  if (existingUser) {
+    return res.status(411).json({
+      message: "Email already taken ",
+    });
+  }
+  const user = new USER(body);
+  await user.save();
+
+  const token = JWT.sign({ user_id: existingUser._id }, JWT_SECRET_KEY, {
+    expiresIn: "1h",
+  });
+  res.status(200).json({
+    message: "User created successfully",
+    token: token,
+  });
+});
+//sign In route
+const SignInSchema = Zod.object({
+  username: string().email(),
+  password: string(),
+});
+UserRouter.post("/signIn", async (req, res) => {
+  const body = req.body;
+  const { success } = SignInSchema.safeParse(req.body);
+  if (!success) {
+    return res.status(411).json({
+      message: "error while logging in",
+    });
+  }
+  const existingUser = await USER.findOne({ username: body.username });
+  if (existingUser) {
+    const token = JWT.sign({ user_id: existingUser._id }, JWT_SECRET_KEY, {
+      expiresIn: "2h",
+    });
+    return res
+      .status(200)
+      .json({ message: "user created Successfully", token: token });
+  }
+  return res.status(411).json({
+    message: "Error while logging in",
+  });
+});
+const UpdatedBodySchema=Zod.object({
+  password:Zod.string().optional(),
+  firstName:Zod.string().optional(),
+  lastName:Zod.string().optional()
+})
+UserRouter.put('/',authMiddleware,async(req,res)=>{
+  const updatedBody=req.body;
+  const {success}=UpdatedBodySchema.safeParse(updatedBody);
+  if(!success){
+   return res.status(411).json({
+      message: "Error while updating information"
+  })
+  }
+  await USER.updateOne({_id:req.user.user_id},req.body);
+  res.json({
+    message: "Updated successfully"
+})
+})
+
+module.exports = UserRouter;
